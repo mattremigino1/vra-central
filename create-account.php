b2a80f660369919f306372ebf93870848938b641
alexbckr <amb3yn@virginia.edu>
2023-04-06 20:40:28 -0400

    styling

--- create-account.php
+++ create-account.php
@@ -1,35 +1,33 @@
-<?php 
-require("new-athlete-handler.php"); 
+<?php
+require("new-athlete-handler.php");
 require("connect-db.php");
 
 $account_created = False;
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST')
-{
-  if (!empty($_POST['createAcctBtn']) && $_POST['createAcctBtn']=="Create Account")
-  {
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  if (!empty($_POST['createAcctBtn']) && $_POST['createAcctBtn'] == "Create Account") {
     $account_created = createAccount(
-      trim($_POST['first_name']), 
-      trim($_POST['last_name']), 
+      trim($_POST['first_name']),
+      trim($_POST['last_name']),
       trim($_POST['emailaddr']),
-      trim($_POST['phone']), 
-      trim($_POST['dob']), 
-      trim($_POST['grad_year']), 
-      trim($_POST['height']), 
+      trim($_POST['phone']),
+      trim($_POST['dob']),
+      trim($_POST['grad_year']),
+      trim($_POST['height']),
       trim($_POST['weight']),
-      trim($_POST['class']), 
-      trim($_POST['boat_side']), 
+      trim($_POST['class']),
+      trim($_POST['boat_side']),
       trim($_POST['2kprM']),
-      trim($_POST['2kprS']), 
+      trim($_POST['2kprS']),
       trim($_POST['pwd']),
-      trim($_POST['pwd_confirm']) 
+      trim($_POST['pwd_confirm'])
     );
     $athlete_id = getAthID();
   }
 }
 if ($account_created) {
   $Message = urlencode($athlete_id);
-  header("Location:login.php?athlete_id=".$Message);
+  header("Location:login.php?athlete_id=" . $Message);
   die;
 }
 
@@ -39,68 +37,92 @@ if ($account_created) {
 
 <!DOCTYPE html>
 <html>
+
 <head>
-  <meta charset="utf-8">   
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">  <!-- required to handle IE -->
-  <meta name="viewport" content="width=device-width, initial-scale=1">  
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge"> <!-- required to handle IE -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Create Account</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">  
-  <link rel="stylesheet" href="activity-styles.css" /> 
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" href="main.css" />
 </head>
+
 <body>
-  
+
   <!-- <?php include('header.html') ?> -->
-  <div class="form-container">  
+  <div class="form-container">
     <h1>Create Your VRA Account</h1>
-   
+
     <!-- what are form inputs -->
     <!-- who will handle the form submission -->
     <!-- how are the request sent -->
-   
-    <form action="create-account.php" method="post">
-      <label>First Name: </label>
-      <input type="text" name="first_name" autofocus required /> <br/>
-      <label>Last Name: </label>
-      <input type="text" name="last_name" required /> <br/>
-      <label>Email: </label><br/>
-      <input type="email" name="emailaddr" required /> <br/>
-      <label>Phone Number: </label>
-      <input type="tel" name="phone" required /> <br/>
-      <label>Date of Birth: </label>
-      <input type="date" name="dob" required /> <br/>
-      <label>Graduation Year: </label>
-      <input type="number" name="grad_year" required /> <br/>
-      <label>Height (in): </label> <br/>
-      <input type="number" name="height" required /> <br/>
-      <label>Weight (lbs): </label> <br/>
-      <input type="number" name="weight" required /> <br/>
-      <label>Class (1, 2, 3, 4): </label> <br/>
-      <input type="text" name="class" required /> <br/>
-      <label>Boat Side (S or P): </label>
-      <input type="text" name="boat_side" required /> <br/>
-      <label>2KPR Minutes: </label>
-      <input type="number" name="2kprM" required /> <br/>
-      <label>2KPR Seconds: </label>
-      <input type="number" name="2kprS" required /> <br/>
-      <!-- set password and confirm password -->
-      <label>Password: </label>
-      <input type="password" name="pwd" required /> <br/>
-      <label>Confirm Password: </label>
-      <input type="password" name="pwd_confirm" required /> <br/>
+
+    <form class="create-account-form">
+      <div class="form-group">
+        <label for="fn">First Name: </label>
+        <input type="text" class="form-control" name="first_name" id="fn" autofocus required />
+      </div>
+      <div class="form-group">
+        <label>Last Name: </label>
+        <input type="text" class="form-control" name="last_name" required />
+      </div>
+      <div class="form-group">
+        <label>Phone Number: </label>
+        <input type="tel" class="form-control" name="phone" required />
+      </div>
+      <div class="form-group">
+        <label>Date of Birth: </label>
+        <input type="date" class="form-control" name="dob" required />
+      </div>
+      <div class="form-group">
+        <label>Graduation Year: </label>
+        <input type="number" class="form-control" name="grad_year" required />
+      </div>
+      <div class="form-group">
+        <label>Height (in): </label> <br />
+        <input type="number" class="form-control" name="height" required />
+      </div>
+      <div class="form-group">
+        <label>Weight (lbs): </label> <br />
+        <input type="number" class="form-control" name="weight" required />
+      </div>
+      <div class="form-group">
+        <label>Class (1, 2, 3, 4): </label> <br />
+        <input type="text" class="form-control" name="class" required />
+      </div>
+      <div class="form-group">
+        <label>Boat Side (S or P): </label>
+        <input type="text" class="form-control" name="boat_side" required />
+      </div>
+      <div class="form-group">
+        <label>2KPR Minutes: </label>
+        <input type="number" class="form-control" name="2kprM" required />
+      </div>
+      <div class="form-group">
+        <label>2KPR Seconds: </label>
+        <input type="number" class="form-control" name="2kprS" required />
+      </div>
+      <div class="form-group">
+        <label>Password: </label>
+        <input type="password" class="form-control" name="pwd" required />
+      </div>
+      <div class="form-group">
+        <label>Confirm Password: </label>
+        <input type="password" class="form-control" name="pwd_confirm" required />
+      </div>
       <input type="submit" name="createAcctBtn" value="Create Account" class="btn btn-primary" />
-      <br> <br> <br> <br>
-      <!-- when the user clicks the button, the form data will be sent to form-handler.php -->
-      <!-- the form data will be sent as a POST request -->
     </form>
-    
     <div class="error-container"></div>
   </div>
 
-  
- 
+
+
   <?php include('footer.html') ?>
-  
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-  </body>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+    crossorigin="anonymous"></script>
+</body>
 
 </html>
--- login.php
+++ login.php
@@ -1,89 +1,94 @@
-<?php 
-    require("connect-db.php"); 
-    session_start();
+<?php
+require("connect-db.php");
+session_start();
 ?>
 
 <!DOCTYPE html>
 <html>
-<head>
-  <meta charset="utf-8">   
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">  <!-- required to handle IE -->
-  <meta name="viewport" content="width=device-width, initial-scale=1">  
-  <title>VRA Login</title> 
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">  
-  <link rel="stylesheet" href="activity-styles.css" /> 
-</head>
-<body>
-  
-  <?php 
-  
-//   include('header.html');
 
-    if(isset($_GET['athlete_id'])) {
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"> <!-- required to handle IE -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>VRA Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="activity-styles.css" />
+</head>
+
+<body>
+
+    <?php
+
+    //   include('header.html');
+    
+    if (isset($_GET['athlete_id'])) {
         echo "New Account Created Succesfully! \n Your Athlete ID is: ";
         echo $_GET['athlete_id'];
         echo "    You need this to login, so make sure to write it down!";
     }
 
-  ?>
+    ?>
 
-  
-  <div>  
-    <h1>VRA Login</h1>
-    <form action="login.php" method="post">
-      Athlete ID: <input type="text" name="athleteID" required /> <br/>
-      Password: <input type="password" name="pwd" required /> <br/>
-      <input type="submit" class="btn btn-primary" value="Login" />
-    </form>
-    <input type="submit" class="btn btn-dark" value="Create Account" onclick="location.href='create-account.php';">
- 
-    <?php
 
-function authenticate()
-{
-    global $db, $mainpage;
+    <div>
+        <h1>VRA Login</h1>
+        <form action="login.php" method="post">
+            Athlete ID: <input type="text" name="athleteID" required /> <br />
+            Password: <input type="password" name="pwd" required /> <br />
+            <input type="submit" class="btn btn-primary" value="Login" />
+        </form>
+        <input type="submit" class="btn btn-dark" value="Create Account" onclick="location.href='create-account.php';">
 
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $athlete_id = htmlspecialchars($_POST['athleteID']);
-        $password = htmlspecialchars($_POST['pwd']);
+        <?php
 
-        // prepare a query to fetch the hashed password for the given username
-        $sql = "SELECT psswrd FROM Passwords WHERE athlete_id = :athlete_id";
-        $stmt = $db->prepare($sql);
-        $stmt->bindValue(':athlete_id', $athlete_id);
-        $stmt->execute();
-        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        function authenticate()
+        {
+            global $db, $mainpage;
 
-        if ($row) {
-            $hash = $row['psswrd'];
+            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                $athlete_id = htmlspecialchars($_POST['athleteID']);
+                $password = htmlspecialchars($_POST['pwd']);
 
-            // hash the password entered by the user
-            $hashed_password = password_hash($password, PASSWORD_BCRYPT);
+                // prepare a query to fetch the hashed password for the given username
+                $sql = "SELECT psswrd FROM Passwords WHERE athlete_id = :athlete_id";
+                $stmt = $db->prepare($sql);
+                $stmt->bindValue(':athlete_id', $athlete_id);
+                $stmt->execute();
+                $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
-            // compare the hashed password with the hash fetched from the database
-            if (password_verify($password, $hash)) {
-                // successfully login, redirect the user to the main page
-                $_SESSION['loggedin'] = true;
-                $_SESSION['athlete_id'] = $athlete_id;
-                header("Location: " . $mainpage);
-            } else {
-                echo "<span class='msg'>Athlete ID and password do not match our record</span> <br/>";
+                if ($row) {
+                    $hash = $row['psswrd'];
+
+                    // hash the password entered by the user
+                    $hashed_password = password_hash($password, PASSWORD_BCRYPT);
+
+                    // compare the hashed password with the hash fetched from the database
+                    if (password_verify($password, $hash)) {
+                        // successfully login, redirect the user to the main page
+                        $_SESSION['loggedin'] = true;
+                        $_SESSION['athlete_id'] = $athlete_id;
+                        header("Location: " . $mainpage);
+                    } else {
+                        echo "<span class='msg'>Athlete ID and password do not match our record</span> <br/>";
+                    }
+                } else {
+                    echo "<span class='msg'>Athlete ID and password do not match our record</span> <br/>";
+                }
             }
-        } else {
-            echo "<span class='msg'>Athlete ID and password do not match our record</span> <br/>";
         }
-    }
-}
 
-$mainpage = "index.php";
-authenticate();
+        $mainpage = "index.php";
+        authenticate();
 
-?>
+        ?>
     </div>
-<?php include('footer.html') ?>
-  
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    
-</body>
-</html>
+    <?php include('footer.html') ?>
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
+
+</body>
+
+</html>
