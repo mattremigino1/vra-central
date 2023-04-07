4a6795ec2befe4b94d2094fde9d0d5bf642bfd70
alexbckr <amb3yn@virginia.edu>
2023-04-06 22:08:56 -0400

    minor styling

--- index.php
+++ index.php
@@ -42,13 +42,15 @@ $myLineup = myLineup($athlete_id);
 
   <title>VRA Central</title>
 
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">  
-  <link rel="icon" type="image/png" href="http://www.cs.virginia.edu/~up3f/cs4750/images/db-icon.png" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
+  <link rel="icon" type="image/png" href="http://www.cs.virginia.edu/~up3f/cs4750/images/db-icon.png" />
+  <link rel="stylesheet" href="main.css" />
 
 </head>
 
-<body>
+<body class='page-body'>
 
   <?php
 
@@ -57,14 +59,18 @@ $myLineup = myLineup($athlete_id);
   ?>
   <div class="container">
     <br>
-    <h3>
+    <h1>
       <?php
       echo "Welcome ";
       echo $name[0];
-      echo ". Your Athlete ID: ";
+      ?>
+    </h1>
+    <h4>
+      <?php
+      echo "Athlete ID: ";
       echo $athlete_id;
       ?>
-    </h3>
+    </h4>
     <br>
 
     <div class="row justify-content-center">
--- login.php
+++ login.php
@@ -13,14 +13,14 @@ session_start();
     <title>VRA Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="activity-styles.css" />
+    <link rel="stylesheet" href="main.css" />
 </head>
 
-<body>
+<body class="page-body">
 
     <?php
 
-    //   include('header.html');
+    // include('header.html');
     
     if (isset($_GET['athlete_id'])) {
         echo "New Account Created Succesfully! \n Your Athlete ID is: ";
@@ -30,59 +30,64 @@ session_start();
 
     ?>
 
-
-    <div>
-        <h1>VRA Login</h1>
-        <form action="login.php" method="post">
-            Athlete ID: <input type="text" name="athleteID" required /> <br />
-            Password: <input type="password" name="pwd" required /> <br />
-            <input type="submit" class="btn btn-primary" value="Login" />
+    <div class="login-form-container">
+        <h1 class='form-title'>VRA Login</h1>
+        <form action="login.php" method="post" class="login-form">
+            <div class="form-group">
+                <label>Athlete ID: </label>
+                <input type="text" class="form-control" name="athleteID" autofocus required />
+            </div>
+            <div class="form-group">
+                <label>Password: </label>
+                <input type="password" class="form-control" name="pwd" autofocus required />
+            </div>
+            <input type="submit" class="login-btn btn btn-primary" value="Login" />
         </form>
-        <input type="submit" class="btn btn-dark" value="Create Account" onclick="location.href='create-account.php';">
+        <input type="submit" class="create-account-btn btn btn-link" value="Create Account" onclick="location.href='create-account.php';">
+    </div>
 
-        <?php
+    <?php
 
-        function authenticate()
-        {
-            global $db, $mainpage;
+    function authenticate()
+    {
+        global $db, $mainpage;
 
-            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-                $athlete_id = htmlspecialchars($_POST['athleteID']);
-                $password = htmlspecialchars($_POST['pwd']);
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $athlete_id = htmlspecialchars($_POST['athleteID']);
+            $password = htmlspecialchars($_POST['pwd']);
 
-                // prepare a query to fetch the hashed password for the given username
-                $sql = "SELECT psswrd FROM Passwords WHERE athlete_id = :athlete_id";
-                $stmt = $db->prepare($sql);
-                $stmt->bindValue(':athlete_id', $athlete_id);
-                $stmt->execute();
-                $row = $stmt->fetch(PDO::FETCH_ASSOC);
+            // prepare a query to fetch the hashed password for the given username
+            $sql = "SELECT psswrd FROM Passwords WHERE athlete_id = :athlete_id";
+            $stmt = $db->prepare($sql);
+            $stmt->bindValue(':athlete_id', $athlete_id);
+            $stmt->execute();
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
-                if ($row) {
-                    $hash = $row['psswrd'];
+            if ($row) {
+                $hash = $row['psswrd'];
 
-                    // hash the password entered by the user
-                    $hashed_password = password_hash($password, PASSWORD_BCRYPT);
+                // hash the password entered by the user
+                $hashed_password = password_hash($password, PASSWORD_BCRYPT);
 
-                    // compare the hashed password with the hash fetched from the database
-                    if (password_verify($password, $hash)) {
-                        // successfully login, redirect the user to the main page
-                        $_SESSION['loggedin'] = true;
-                        $_SESSION['athlete_id'] = $athlete_id;
-                        header("Location: " . $mainpage);
-                    } else {
-                        echo "<span class='msg'>Athlete ID and password do not match our record</span> <br/>";
-                    }
+                // compare the hashed password with the hash fetched from the database
+                if (password_verify($password, $hash)) {
+                    // successfully login, redirect the user to the main page
+                    $_SESSION['loggedin'] = true;
+                    $_SESSION['athlete_id'] = $athlete_id;
+                    header("Location: " . $mainpage);
                 } else {
                     echo "<span class='msg'>Athlete ID and password do not match our record</span> <br/>";
                 }
+            } else {
+                echo "<span class='msg'>Athlete ID and password do not match our record</span> <br/>";
             }
         }
+    }
 
-        $mainpage = "index.php";
-        authenticate();
+    $mainpage = "index.php";
+    authenticate();
 
-        ?>
-    </div>
+    ?>
     <?php include('footer.html') ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
