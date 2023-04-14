f19ac82c03e775b13459aef49bdec2e2a35368e3
Maajid Husain <77751861+maajidhusain@users.noreply.github.com>
2023-04-14 16:17:39 -0400

    athlete-directory and handler

---
+++ athlete-directory-handler.php
@@ -0,0 +1,49 @@
+<?php
+
+function selectAllAthletes() {
+    // db
+    global $db;
+    // query
+    $query = "SELECT * FROM Athlete";
+    // prepare
+    $statement = $db->prepare($query);
+    // execute
+    $statement->execute();
+    // retrieve
+    $results = $statement->fetchAll(); //fetch() will retrieve only first row fetchAll will retrieve all rows
+    // close cursor
+    $statement->closeCursor();
+
+    return $results;
+}
+
+function deleteAthlete($athlete_to_delete) {
+
+        // db
+        global $db;
+        // query
+        $query = "DELETE FROM Athlete WHERE name=:athlete_to_delete";
+        // prepare
+        $statement = $db->prepare($query);
+        $statement->bindValue(':athlete_to_delete', $friend_to_delete);
+        // execute
+        $statement->execute();
+        // close cursor
+        $statement->closeCursor();
+}
+
+function getAthleteByName($athlete_to_update) {
+    global $db;
+    $query = "SELECT * FROM Athlete WHERE first_name=:athlete_to_update";
+    // prepare
+    $statement = $db->prepare($query);
+    $statement->bindValue(':athlete_to_update', $athlete_to_update);
+    // execute
+    $statement->execute();
+    $results = $statement->fetchAll(); //fetch() will retrieve only first row fetchAll will retrieve all rows
+    // close cursor
+    $statement->closeCursor();
+    return $results;
+}
+
+?>
---
+++ athlete-directory.php
@@ -0,0 +1,111 @@
+<?php
+require("connect-db.php"); 
+// include("connect-db.php");
+
+require("athlete-directory-handler.php");
+$athletes = selectAllAthletes();
+
+if (isset($_POST['submit'])) {
+    $search_name = $_POST['search_name'];
+    $athletes = getAthleteByName($search_name);
+  } else {
+    $athletes = selectAllAthletes();
+  }
+
+?>
+
+<!-- 1. create HTML5 doctype -->
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">  
+  
+  <!-- 2. include meta tag to ensure proper rendering and touch zooming -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="author" content="your name">
+  <meta name="description" content="include some description about your page">  
+    
+  <title>VRA Central Athlete Directory</title>
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link rel="icon" type="image/png" href="http://www.cs.virginia.edu/~up3f/cs4750/images/db-icon.png" />
+
+       
+</head>
+
+<body>
+
+<?php include("header.html") ?>
+
+<!-- Add form to POST to athlete-directory.php and filter by name -->
+
+<div class="container">
+  <form action="athlete-directory.php" method="post">
+    <div class="row justify-content-center">
+      <div class="col-4">
+        <input type="text" class="form-control" name="search_name" placeholder="Search by First Name">
+      </div>
+      <div class="col-2">
+        <input type="submit" class="btn btn-primary" name="submit" value="Search">
+      </div>
+    </div>
+  </form>
+
+<div class="container">
+  <h1>Athlete Directory</h1>  
+  <div class="row justify-content-center">  
+        <table class="w3-table w3-bordered w3-card-4 center" style="width:70%">
+        <div class="container">
+          <thead>
+          <tr style="background-color:#B0B0B0">
+            <th>Athlete ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Phone Number</th>
+            <th>Date of Birth</th>
+            <th>Grad Year</th>
+            <th>Height</th>
+            <th>Athletic Weight</th>
+            <th>Class</th>
+            <th>Boat Side</th>
+            <th>2K PR</th>
+            <th>G8</th>
+            <th>Age</th>
+          </tr>
+          </thead>
+        <?php foreach ($athletes as $item): ?>
+          <tr>
+             <td><?php echo $item['athlete_id']; ?></td>
+             <td><?php echo $item['first_name']; ?></td>        
+             <td><?php echo $item['last_name']; ?></td>  
+             <td><?php echo $item['email']; ?></td>
+             <td><?php echo $item['phone_number']; ?></td>
+             <td><?php echo $item['dob']; ?></td>
+             <td><?php echo $item['grad_year']; ?></td>
+             <td><?php echo $item['height']; ?></td>
+             <td><?php echo $item['athletic_weight']; ?></td>
+             <td><?php echo $item['class']; ?></td>
+             <td><?php echo $item['boat_side']; ?></td>
+             <td><?php echo $item['2k_pr']; ?></td>
+             <td><?php echo $item['g8']; ?></td>
+             <td><?php echo $item['age']; ?></td>            
+          </tr>
+        <?php endforeach; ?>
+        </table>
+  </div>   
+
+
+
+
+
+
+
+
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> 
+  
+</body>
+
+</html>
