9defc8490a5490f5828cc0970f541005a2276502
Maajid Husain <77751861+maajidhusain@users.noreply.github.com>
2023-04-15 14:21:53 -0400

    update

--- coach-athlete-directory.php
+++ coach-athlete-directory.php
@@ -43,7 +43,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
   <link rel="icon" type="image/png" href="http://www.cs.virginia.edu/~up3f/cs4750/images/db-icon.png" />
 
-       
 </head>
 
 <body>
