e73c5c5ea9f3fa77640543925b44011b81347b04
alexbckr <amb3yn@virginia.edu>
2023-05-01 08:48:55 -0400

    coach index and athlete directory

--- coach-athlete-directory.php
+++ coach-athlete-directory.php
@@ -37,21 +37,19 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
   <link rel="icon" type="image/png" href="http://www.cs.virginia.edu/~up3f/cs4750/images/db-icon.png" />
   <link rel="stylesheet" href="main.css" />
 </head>
 
 <body>
-  <div class="page-body">
-    <?php
-    include("Cheader.html");
-    ?>
+<div class="page-body">
+  <?php include("Cheader.html") ?>
 
-    <!-- Add form to POST to athlete-directory.php and filter by name -->
-
-    <div class="container">
-      <h1 class="page-title">Athlete Directory</h1>
-      <form action="athlete-directory.php" method="post" class="filter-form-container">
+  <!-- Add form to POST to athlete-directory.php and filter by name -->
+  <div class="container">
+  <h2 class="page-title">Coach Athlete Directory</h2>
+    <form action="coach-athlete-directory.php" method="post" class="filter-form-container">
         <select name="athlete" class='form-control'>
           <option value="">-- Select --</option>
           <?php foreach ($all_athletes as $item): ?>
@@ -62,77 +60,86 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         </select>
         <input class="btn btn-primary" name="selectAthlete" type="submit" value="View Athlete" />
       </form>
-
       <table class="table table-hover table-striped table-sm athlete-directory-table">
-        <thead>
-          <tr class="athlete-directory-tr">
-            <th>ID</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Email</th>
-            <th>Phone Number</th>
-            <th>Date of Birth</th>
-            <th>Grad Year</th>
-            <th>Height</th>
-            <th>Athletic Weight</th>
-            <th>Class</th>
-            <th>Boat Side</th>
-            <th>2K PR</th>
-            <th>G8</th>
-            <th>Age</th>
-          </tr>
-        </thead>
-        <?php foreach ($athletes as $item): ?>
-          <tr>
-            <td>
-              <?php echo $item['athlete_id']; ?>
-            </td>
-            <td>
-              <?php echo $item['first_name']; ?>
-            </td>
-            <td>
-              <?php echo $item['last_name']; ?>
-            </td>
-            <td>
-              <?php echo $item['email']; ?>
-            </td>
-            <td>
-              <?php echo $item['phone_number']; ?>
-            </td>
-            <td>
-              <?php echo $item['date_of_birth']; ?>
-            </td>
-            <td>
-              <?php echo $item['grad_year']; ?>
-            </td>
-            <td>
-              <?php echo $item['height']; ?>
-            </td>
-            <td>
-              <?php echo $item['ath_weight']; ?>
-            </td>
-            <td>
-              <?php echo $item['class']; ?>
-            </td>
-            <td>
-              <?php echo $item['boat_side']; ?>
-            </td>
-            <td>
-              <?php echo $item['twoKPR']; ?>
-            </td>
-            <td>
-              <?php echo $item['g8']; ?>
-            </td>
-            <td>
-              <?php echo $item['age']; ?>
-            </td>
-          </tr>
-        <?php endforeach; ?>
+        <div class="container">
+          <thead>
+            <tr>
+              <th>Athlete ID</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Email</th>
+              <th>Phone Number</th>
+              <th>Date of Birth</th>
+              <th>Grad Year</th>
+              <th>Height</th>
+              <th>Athletic Weight</th>
+              <th>Class</th>
+              <th>Boat Side</th>
+              <th>2K PR</th>
+              <th>G8</th>
+              <th>Age</th>
+            </tr>
+          </thead>
+          <?php foreach ($athletes as $item): ?>
+            <tr>
+              <td>
+                <?php echo $item['athlete_id']; ?>
+              </td>
+              <td>
+                <?php echo $item['first_name']; ?>
+              </td>
+              <td>
+                <?php echo $item['last_name']; ?>
+              </td>
+              <td>
+                <?php echo $item['email']; ?>
+              </td>
+              <td>
+                <?php echo $item['phone_number']; ?>
+              </td>
+              <td>
+                <?php echo $item['date_of_birth']; ?>
+              </td>
+              <td>
+                <?php echo $item['grad_year']; ?>
+              </td>
+              <td>
+                <?php echo $item['height']; ?>
+              </td>
+              <td>
+                <?php echo $item['ath_weight']; ?>
+              </td>
+              <td>
+                <?php echo $item['class']; ?>
+              </td>
+              <td>
+                <?php echo $item['boat_side']; ?>
+              </td>
+              <td>
+                <?php echo $item['twoKPR']; ?>
+              </td>
+              <td>
+                <?php echo $item['g8']; ?>
+              </td>
+              <td>
+                <?php echo $item['age']; ?>
+              </td>
+            </tr>
+          <?php endforeach; ?>
+      </table>
+
+
+
+
+
+
+
+
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
       integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
       crossorigin="anonymous"></script>
-
+          </div>
 </body>
 
 </html>
